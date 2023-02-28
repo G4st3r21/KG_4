a6f580b0cfbcbe9f7514ee93ca3fe6376b9ccc87
G4st3r21 <aporyadin003@gmail.com>
2023-02-28 22:04:10 +0300

    Рабочая версия

--- RenderEngine/RenderEngine.py
+++ RenderEngine/RenderEngine.py
@@ -1,4 +1,5 @@
 from PySide6.QtGui import QPainter
+
 from Math.Matrix import Matrix
 from Math.Vector3F import Vector3F
 from Model.Model import Model
@@ -6,7 +7,7 @@ from .GraphicConveyor import rotate_scale_translate, multiply_matrix4_by_vector3
 from RenderEngine import Camera
 
 
-def render(qp: QPainter, full_polygons: bool, camera: Camera, mesh: Model, width, height):
+def render(qp: QPainter, polygon_quality: int, camera: Camera, mesh: Model, width, height):
     model_matrix = rotate_scale_translate()
     view_matrix = camera.get_view_matrix()
     projection_matrix = camera.get_projection_matrix()
@@ -14,7 +15,9 @@ def render(qp: QPainter, full_polygons: bool, camera: Camera, mesh: Model, width
     model_view_projection_matrix = model_matrix
     model_view_projection_matrix = Matrix.multiply(model_view_projection_matrix, view_matrix)
     model_view_projection_matrix = Matrix.multiply(model_view_projection_matrix, projection_matrix)
+    # mesh.recalculate_poly_avg_dz(cam_pos=camera.position)
 
+    all_res_points = []
     for polygon in mesh.polygons:
         n_vertices_in_polygon = len(polygon.get_vertex_indices())
         result_points = []
@@ -23,23 +26,13 @@ def render(qp: QPainter, full_polygons: bool, camera: Camera, mesh: Model, width
             vertex_vecmath = Vector3F(vertex.x, vertex.y, vertex.z)
             result_point = multiply_matrix4_by_vector3(model_view_projection_matrix, vertex_vecmath, width, height)
             result_points.append(result_point)
+        all_res_points.append(result_points)
 
-        if full_polygons:
-            vertex_in_polygon_ind = 1
-
-            while vertex_in_polygon_ind < n_vertices_in_polygon:
-                qp.drawLine(
-                    int(result_points[vertex_in_polygon_ind - 1][0]),
-                    int(-result_points[vertex_in_polygon_ind - 1][1]),
-                    int(result_points[vertex_in_polygon_ind][0]),
-                    int(-result_points[vertex_in_polygon_ind][1]),
-                )
-                vertex_in_polygon_ind += 1
-
-        if n_vertices_in_polygon > 0:
+    for result_points in all_res_points:
+        for line_count in range(polygon_quality):
             qp.drawLine(
-                int(result_points[n_vertices_in_polygon - 1][0]),
-                int(-result_points[n_vertices_in_polygon - 1][1]),
-                int(result_points[0][0]),
-                int(-result_points[0][1])
+                int(result_points[line_count - 1][0]),
+                int(-result_points[line_count - 1][1]),
+                int(result_points[line_count][0]),
+                int(-result_points[line_count][1]),
             )
